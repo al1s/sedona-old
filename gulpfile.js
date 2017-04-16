a53260a098665dc354eb6c59202895b1e3f8471c
Alstof <alstof@gmail.com>
2017-04-16 14:25:33 -0700

    Add logo

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,7 @@ to create package.js. It will contain all required modules from gulpfile.json.
 
 5. Type 'Gulp' and ster developing
 */
+/*jshint esversion: 6 */
 
 /* Needed gulp config */
 const gulp = require('gulp');  
@@ -34,7 +35,7 @@ const browserSync = require('browser-sync');
 const neat = require('node-neat');
 const postcss = require('gulp-postcss');
 const mqpacker = require('css-mqpacker');
-const autoprefixer = require('autoprefixer')
+const autoprefixer = require('autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const reload = browserSync.reload;
 const jshint = require('gulp-jshint');
@@ -43,7 +44,8 @@ const changed = require('gulp-changed');
 
 /* Setup scss path */
 var paths = {
-  scss: ['./sass/*.scss', './sass/*.css']
+  sass: ['./stylesheets/sass/**/*.scss'],
+  scss: ['./stylesheets/scss/style.scss']
 };
 
 /* Scripts task */
@@ -67,13 +69,38 @@ gulp.task('scripts', function() {
 });
 
 /* Sass task */
+gulp.task('sass1', function () {  
+  gulp.src('./stylesheets/scss/style.scss', {base: 'stylesheets/'})
+    .pipe(changed('stylesheets'))
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: [paths.sass].concat()
+          })
+          .on('error', sass.logError))
+    .pipe(postcss([                                         // делаем постпроцессинг
+        autoprefixer({ browsers: ['last 2 version', '> 5%'] }),     // автопрефиксирование
+        mqpacker({ sort: true }),                           // объединение медиавыражений
+     ]))   
+    .pipe(sourcemaps.write('../stylesheets/maps'))
+    .pipe(gulp.dest('css'))
+    .pipe(sourcemaps.init({loadMaps:true}))   // create maps from scss *sourcemaps* not the css
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('css'))
+    /* Reload the browser CSS after every change */
+    .pipe(reload({stream:true}));
+});
+
+
+/* Sass task */
 gulp.task('sass', function () {  
     gulp.src(['./stylesheets/scss/style.scss', './stylesheets/scss/*.css'])
     .pipe(changed('stylesheets'))
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({
-        includePaths: ['scss'].concat(neat)
+        includePaths: [paths.sass].concat(neat)
     }))
     .pipe(postcss([                                         // делаем постпроцессинг
         autoprefixer({ browsers: ['last 2 version', '> 5%'] }),     // автопрефиксирование
@@ -87,6 +114,21 @@ gulp.task('sass', function () {
     /* Reload the browser CSS after every change */
     .pipe(reload({stream:true}));
 });
+
+
+gulp.task('sass-compile', function() {
+    return gulp.src('./stylesheets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError)) 
+    .pipe(postcss([                                         // делаем постпроцессинг
+        autoprefixer({ browsers: ['last 2 version', '> 5%'] }),     // автопрефиксирование
+        mqpacker({ sort: true }),                           // объединение медиавыражений
+     ]))   
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+        stream: true
+    }));
+});
+
 
 /* Reload task */
 gulp.task('bs-reload', function () {
@@ -120,12 +162,56 @@ gulp.task('lintHTML', function() {
     .pipe(jshint.reporter('fail'));
 });
 
+/* Compile SASS and later minify it keeping mapping throughout */
+gulp.task('sass-compile1',function(){
+    return gulp.src('./scss/style.scss')
+        .pipe(maps.init())          // create maps from scss partials
+        .pipe(sass())
+        .pipe(maps.write('./'))     // this path is relative to the output directory
+        .pipe(gulp.dest('./css'));  // this is the output directory
+});
+
+// a task in here called concat-css  concats all css files and changes name to output.css
+gulp.task('minify-css',['concat-css'], function(){
+    return gulp.src('css/output.css')
+        .pipe(maps.init({loadMaps:true}))   // create maps from scss *sourcemaps* not the css
+        .pipe(cleanCSS())
+        .pipe(rename('output.min.css'))
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('css'));
+});
+
+/* Compile Javascript and later minify it keeping mapping throughout */
+  gulp.task("concatScripts", ['lintScripts'], function() {
+    return gulp.src([
+      config.paths.dep + '/jquery-1.11.3/jquery-1.11.3.js',
+      config.paths.dep + '/bootstrap/bootstrap.js',
+      config.paths.src + '/js/plugins.js',
+      config.paths.src + '/js/script.js'
+    ])
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('script.js'))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest(config.paths.dist + '/js'));
+  });
+
+  gulp.task("minifyScripts", ["concatScripts"], function() {
+    return gulp.src(config.paths.dist + "/js/script.js")
+    .pipe($.sourcemaps.init({loadMaps: true}))
+    .pipe($.uglify())
+    .pipe($.rename('script.min.js'))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest(config.paths.dist + '/js'));
+  });
+
+
+
 /* Watch scss, js and html files, doing different things with each. */
 gulp.task('default', ['sass', 'browser-sync'], function () {
     /* Watch scss, run the sass task on change. */
-    gulp.watch(['./stylesheets/scss/*.scss', './stylesheets/scss/**/*.scss'], ['sass'])
+    gulp.watch(['./stylesheets/scss/*.scss', './stylesheets/scss/**/*.scss'], ['sass']);
     /* Watch app.js file, run the scripts task on change. */
-    gulp.watch(['./js/*.js'], ['scripts'])
+    gulp.watch(['./js/*.js'], ['scripts']);
     /* Watch .html files, run the bs-reload task on change. */
     gulp.watch(['./*.html'], ['bs-reload']);
 });
